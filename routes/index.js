74952f37045ba1db16fb564e73228af917fe7dc8
Jordan1211 <gre20057@byui.edu>
2022-11-17 11:44:22 -0700

    testing nathans graphql route

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,23 @@
-const express = require("express");
-const { ensureAuth, ensureGuest } = require("../middleware/auth");
+const express = require('express');
+const { ensureAuth, ensureGuest } = require('../middleware/auth');
 const router = express.Router();
+const schema = require('./schema');
 
 // @desc Login/Landing page
 // @route GET /
-router.get("/", ensureGuest, (req, res) => {
-  res.render("login", { layout: "login" });
+router.get('/', ensureGuest, (req, res) => {
+  res.render('login', { layout: 'login' });
 });
 
 // @desc Dashboard
 // @route GET /dashboard
-router.get("/Dashboard", ensureAuth, (req, res) => {
+router.get('/Dashboard', ensureAuth, (req, res) => {
   console.log(req.user);
-  res.render("dashboard", {
+  res.render('dashboard', {
     name: req.user.firstName,
   });
 });
+
+router.use('/graphql', schema);
 
 module.exports = router;
