60a7a72b8ae889cf5e7f4a9f7ba9c07463e0de14
Austin Donovan <iskarr@Austins-MacBook-Pro.local>
2022-11-22 17:26:37 -0700

    updated user to User

--- middleware/loadUser.js
+++ middleware/loadUser.js
@@ -1,5 +1,5 @@
 const appConfig = require("../config/app");
-const User = require("../models/user");
+const User = require("../models/User");
 
 const loadUser = async (req, res, next) => {
   if (!req.headers.authorization) {
