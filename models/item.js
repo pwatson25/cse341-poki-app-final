43718d37714ef487717d493654cf7bf4167cd03a
Jordan1211 <gre20057@byui.edu>
2022-11-22 17:29:59 -0700

    added models

---
+++ models/item.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const itemSchema = new Schema({
+  id: Number,
+  name: String,
+  cost: Number,
+  fling_power: Number,
+  fling_effect: {
+    name: String,
+    url: String,
+  },
+  attributes: {
+    name: String,
+    url: String,
+  },
+  effect_entries: {
+    effect: String,
+    language: {
+      name: String,
+      url: String,
+    },
+    short_effect: String,
+  },
+  held_by_pokemon: {
+    name: String,
+    url: String,
+  },
+  baby_trigger_for: {
+    name: String,
+    url: String,
+  },
+  machines: {
+    name: String,
+    url: String,
+  },
+});
+
+module.exports = mongoose.model('item', itemSchema);
