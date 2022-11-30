e642024f467bf181eae8405e55c176f76d45a7e2
Jordan1211 <gre20057@byui.edu>
2022-11-29 20:22:19 -0700

    revert to previous working version

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,9 @@ const router = express.Router();
 
 const schema = require('./schema');
 const authorizationRoutes = require('./authorization');
-const loadUser = require('../middleware/loadUser');
+// const loadUser = require('../middleware/loadUser');
 
-router.use([loadUser]);
+// router.use([loadUser]);
 router.use('/graphql', schema);
 router.use('/authorization', authorizationRoutes);
 // router.use("/authorization", schema);
