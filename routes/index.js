2747da9e2c67bd0581cb30fd5b975dc2c144787c
Jordan1211 <gre20057@byui.edu>
2022-11-29 19:37:52 -0700

    update middleware

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,9 @@ const router = express.Router();
 
 const schema = require('./schema');
 const authorizationRoutes = require('./authorization');
+const loadUser = require('../middleware/loadUser');
 
+router.use([loadUser]);
 router.use('/graphql', schema);
 router.use('/authorization', authorizationRoutes);
 // router.use("/authorization", schema);
